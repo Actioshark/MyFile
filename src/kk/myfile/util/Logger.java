36f3b0a8a25cb2cd2f76b2b03a18014adf4cbfad
actionshark <sunhongkao@qq.com>
2017-05-01 14:41:09 +0800

    修改打印方法

--- src/kk/myfile/util/Logger.java
+++ src/kk/myfile/util/Logger.java
@@ -4,24 +4,34 @@ import android.util.Log;
 
 public class Logger {
 	public static boolean DEBUG_ENABLE = true;
+	
+	public static String DEF_TAG = "DEBUG";
+	
+	private static void insertElement(StringBuilder sb, StackTraceElement element) {
+		sb.append(element.getFileName()).append('-').append(element.getLineNumber())
+			.append(':').append(element.getMethodName()).append("()\n");
+	}
 
-	public static void print(String tag, Throwable throwable, Object... args) {
+	public static void prints(String tag, Throwable throwable, int trackIndex, Object... args) {
 		if (!DEBUG_ENABLE) {
 			return;
 		}
 
 		if (tag == null) {
-			tag = "DEBUG";
+			tag = DEF_TAG;
 		}
 
 		StringBuilder sb = new StringBuilder();
 
 		StackTraceElement[] stack = new Throwable().getStackTrace();
-		if (stack.length > 1) {
-			StackTraceElement element = stack[1];
-
-			sb.append(element.getFileName()).append('-').append(element.getLineNumber())
-				.append(':').append(element.getMethodName()).append("()\n");
+		if (trackIndex >= 0 && trackIndex < stack.length) {
+			insertElement(sb, stack[trackIndex]);
+		} else {
+			sb.append("-------------------------------\n");
+			for (int i = stack.length - 1; i > 0; i--) {
+				insertElement(sb, stack[i]);
+			}
+			sb.append("-------------------------------\n");
 		}
 
 		for (Object obj : args) {
@@ -30,32 +40,12 @@ public class Logger {
 
 		Log.d(tag, sb.toString());
 
-		Log.d(tag, "", throwable);
+		if (throwable != null) {
+			Log.d(tag, "", throwable);
+		}
 	}
 
 	public static void print(String tag, Object... args) {
-		if (!DEBUG_ENABLE) {
-			return;
-		}
-
-		if (tag == null) {
-			tag = "DEBUG";
-		}
-
-		StringBuilder sb = new StringBuilder();
-
-		StackTraceElement[] stack = new Throwable().getStackTrace();
-		if (stack.length > 1) {
-			StackTraceElement element = stack[1];
-
-			sb.append(element.getFileName()).append('-').append(element.getLineNumber())
-				.append(':').append(element.getMethodName()).append("()\n");
-		}
-
-		for (Object obj : args) {
-			sb.append(String.valueOf(obj)).append(' ');
-		}
-
-		Log.d(tag, sb.toString());
+		prints(tag, null, 2, args);
 	}
 }
